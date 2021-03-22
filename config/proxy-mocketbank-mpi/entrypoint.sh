347281a2a974b72d214611cdc746552f241b4a1e
Ivan Panteleev <ispanteleev@gmail.com>
2021-03-22 18:23:49 +0300

    Values adapted for CP usage

--- config/proxy-mocketbank-mpi/entrypoint.sh
+++ config/proxy-mocketbank-mpi/entrypoint.sh
@@ -4,6 +4,6 @@ set -ue
 java \
     "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
     -jar \
-    /opt/proxy-mocketbank-mpi/proxy-mocketbank-mpi.jar \
+    /opt/proxy-mocketbank-mpi-vulnerable/proxy-mocketbank-mpi-vulnerable.jar \
     --server.port=8080 \
     ${@}
